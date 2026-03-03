598cd5381bf13e3765a4ea5dbc8971f2a40496e9
enlith <enlith@gmail.com>
2026-03-03 21:51:05 +0100

    Optimize kernel config for RK3399-T / Rock 4 SE hardware
    - Enable RK3399 SoC drivers (PM domains, IOMMU, thermal, SARADC)
    - Enable CPU frequency scaling with big.LITTLE governors
    - Enable Mali T860MP4 GPU via panfrost DRM driver
    - Enable HDMI, MIPI DSI display output
    - Enable PCIe host for M.2 slot
    - Enable USB 3.0 DWC3 and USB-C Type-C support
    - Enable eMMC/SD (MMC DW Rockchip, SDHCI Arasan)
    - Enable WiFi/BT brcmfmac for AP6256 module
    - Enable GMAC Ethernet, I2C, SPI, GPIO Rockchip drivers
    - Enable RK808 PMIC (regulator, RTC, clocks)
    - Enable ZRAM with zstd compression
    - Keep virtio options for QEMU emulation
    - Increase ZRAM from 512M/lz4 to 1024M/zstd

--- config/makekernel.sh
+++ config/makekernel.sh
@@ -20,28 +20,100 @@ echo "${BUILD}" > ${CWD}/config/release
 KERNELDIR="KERNEL-${BUILD}"
 mkdir -p "${KERNELDIR}"
 
-scripts/config -e CONFIG_MACVLAN
-scripts/config -e CONFIG_VIRTIO_NET
-scripts/config -e CONFIG_NLMON
-scripts/config -d CONFIG_VT_HW_CONSOLE_BINDING
-scripts/config -e CONFIG_SERIAL_AMBA_PL011
-scripts/config -e CONFIG_SERIAL_AMBA_PL011_CONSOLE
-scripts/config -e CONFIG_VIRTIO_CONSOLE
-scripts/config -e CONFIG_HW_RANDOM
-scripts/config -e CONFIG_HW_RANDOM_VIRTIO
+# --- RK3399 SoC / Rock 4 SE hardware ---
+scripts/config -e CONFIG_ARCH_ROCKCHIP
+scripts/config -e CONFIG_ROCKCHIP_PM_DOMAINS
+scripts/config -e CONFIG_ROCKCHIP_IOMMU
+scripts/config -e CONFIG_ROCKCHIP_IODOMAIN
+scripts/config -e CONFIG_ROCKCHIP_THERMAL
+scripts/config -e CONFIG_ROCKCHIP_SARADC
+scripts/config -e CONFIG_ROCKCHIP_TIMER
+
+# CPU frequency scaling (big.LITTLE A72+A53)
+scripts/config -e CONFIG_ARM_ROCKCHIP_CPUFREQ
+scripts/config -e CONFIG_CPUFREQ_DT
+scripts/config -e CONFIG_CPU_FREQ_GOV_ONDEMAND
+scripts/config -e CONFIG_CPU_FREQ_GOV_SCHEDUTIL
+scripts/config -e CONFIG_CPU_FREQ_GOV_CONSERVATIVE
+scripts/config -e CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND
+scripts/config -e CONFIG_ENERGY_MODEL
+
+# GPU - Mali T860MP4
 scripts/config -e CONFIG_DRM
-scripts/config -e CONFIG_DRM_VIRTIO_GPU
+scripts/config -e CONFIG_DRM_ROCKCHIP
+scripts/config -e CONFIG_DRM_PANFROST
+
+# Display / HDMI
+scripts/config -e CONFIG_DRM_DW_HDMI
+scripts/config -e CONFIG_DRM_DW_HDMI_CEC
+scripts/config -e CONFIG_ROCKCHIP_VOP
+scripts/config -e CONFIG_DRM_DISPLAY_CONNECTOR
+scripts/config -e CONFIG_DRM_DW_MIPI_DSI
+
+# PCIe (M.2 slot)
+scripts/config -e CONFIG_PCIEPORT
+scripts/config -e CONFIG_PCIE_ROCKCHIP_HOST
+scripts/config -e CONFIG_PHY_ROCKCHIP_PCIE
+
+# USB 3.0 / USB-C
+scripts/config -e CONFIG_USB_DWC3
+scripts/config -e CONFIG_USB_DWC3_OF_SIMPLE
+scripts/config -e CONFIG_USB_XHCI_HCD
+scripts/config -e CONFIG_PHY_ROCKCHIP_TYPEC
+scripts/config -e CONFIG_TYPEC
+scripts/config -e CONFIG_TYPEC_FUSB302
+
+# eMMC / SD card
+scripts/config -e CONFIG_MMC_DW
+scripts/config -e CONFIG_MMC_DW_ROCKCHIP
+scripts/config -e CONFIG_MMC_SDHCI_OF_ARASAN
+
+# WiFi/BT (AP6256 - brcmfmac)
+scripts/config -e CONFIG_BRCMFMAC
+scripts/config -m CONFIG_BRCMFMAC_SDIO
+scripts/config -e CONFIG_BT_HCIUART
+scripts/config -e CONFIG_BT_BCM
+
+# Ethernet (GMAC)
+scripts/config -e CONFIG_STMMAC_ETH
+scripts/config -e CONFIG_DWMAC_ROCKCHIP
+
+# Audio
+scripts/config -e CONFIG_SND_SOC_ROCKCHIP
+scripts/config -e CONFIG_SND_SOC_ROCKCHIP_I2S
+scripts/config -e CONFIG_SND_SOC_RT5651
+
+# I2C / SPI / GPIO
+scripts/config -e CONFIG_I2C_RK3X
+scripts/config -e CONFIG_SPI_ROCKCHIP
+scripts/config -e CONFIG_PINCTRL_ROCKCHIP
+scripts/config -e CONFIG_GPIO_ROCKCHIP
+
+# Power / PMIC (RK808)
+scripts/config -e CONFIG_MFD_RK808
+scripts/config -e CONFIG_REGULATOR_RK808
+scripts/config -e CONFIG_RTC_DRV_RK808
+scripts/config -e CONFIG_COMMON_CLK_RK808
+scripts/config -e CONFIG_ROCKCHIP_EFUSE
+
+# Watchdog / RTC
+scripts/config -e CONFIG_DW_WATCHDOG
 scripts/config -e CONFIG_RTC_CLASS
-scripts/config -e CONFIG_RTC_DRV_PL031
+
+# ZRAM
+scripts/config -e CONFIG_ZRAM
+scripts/config -e CONFIG_ZSMALLOC
+scripts/config -e CONFIG_CRYPTO_LZ4
+
+# Virtio (QEMU emulation support)
 scripts/config -e CONFIG_VIRTIO_PCI
 scripts/config -e CONFIG_VIRTIO_MMIO
-scripts/config -e CONFIG_VIRTIO_MMIO_CMDLINE_DEVICES
-scripts/config -e CONFIG_MAILBOX
-scripts/config -e CONFIG_AHCI
-scripts/config -e CONFIG_PCIEPORT
+scripts/config -e CONFIG_VIRTIO_NET
+scripts/config -e CONFIG_VIRTIO_CONSOLE
 scripts/config -e CONFIG_VIRTIO_INPUT
-scripts/config -e CONFIG_VIRT_DRIVERS
-scripts/config -e CONFIG_VIRTIO_MEM
+scripts/config -e CONFIG_DRM_VIRTIO_GPU
+scripts/config -e CONFIG_HW_RANDOM_VIRTIO
+scripts/config -e CONFIG_MAILBOX
 
 yes "" | make -j ${CPUS} ARCH=arm64 KERNELRELEASE="${BUILD}" Image.gz modules dtbs
 
